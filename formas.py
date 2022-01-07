aafbdd6cfeafdf7e590ee598bde8dad81b81c150
Eduardo <eduardopaivadias@gmail.com>
2022-01-07 20:30:20 -0300

    Teste GithubActions

--- formas.py
+++ formas.py
@@ -1,32 +1,32 @@
 import math
 
 class Formas:
-  def areaCirculo(double raio):
+  def areaCirculo(raio):
     return math.pow(raio, 2) * math.pi()
 
-  def volumeEsfera(double raio):
+  def volumeEsfera(raio):
     return (4/3) * math.pi() * math.pow(raio, 3)
 
-  def areaQuadrado(double lado):
+  def areaQuadrado(lado):
     return lado*lado
 
-  def volumeCubo(double lado):
+  def volumeCubo(lado):
     return lado*lado*lado
 
-  def areaTriangulo(double base, double altura):
+  def areaTriangulo(base, altura):
     return (base*altura)/2
 
-  def volumePiramide(double base, double altura):
+  def volumePiramide(base, altura):
     return  (math.pow(base, 2) * altura)/3
 
-  def areaTetraedro(double aresta):
+  def areaTetraedro(aresta):
     return (math.pow(aresta,2) * math.sqrt(3))
 
-  def volumeTetraedro(double aresta):
+  def volumeTetraedro(aresta):
     return (math.pow(aresta,3) * math.sqrt(2))/12
 
-  def volumeCilindro(double raio, double altura):
+  def volumeCilindro(raio, altura):
     return math.pow(raio,2) * math.pi() * altura
 
-  def areaRetangulo(double base, double altura):
+  def areaRetangulo(base, altura):
     return base*altura
